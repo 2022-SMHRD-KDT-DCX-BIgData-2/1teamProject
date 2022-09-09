9eb77bfcf0d485ce13be4e09dc6656b0d9d97095
Jiyoung Lee <dlwldud5107@naver.com>
2022-09-09 18:01:56 +0900

    banner js수정

--- script/script.js
+++ script/script.js
@@ -45,3 +45,54 @@ addEventListener("scroll", () => {
     scroll_input.style.display = "none";
   }
 });
+/**
+ * 배너 동작 로직
+ */
+const bannerBox = document.querySelector("#bannerBox > ul");
+const bannerItem = document.querySelectorAll(".banner_item");
+//배열 반환
+
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const size = bannerItem[0].clientWidth;
+//패딩을 포함한 넓이를 가지고오는 기능
+//해당 요소의 길이를 가지고 온다
+let counter = 0;
+
+// addEventListener
+// 이벤트를 동작시킵니다
+
+nextBtn.addEventListener("click", () => {
+  if (counter < bannerItem.length - 1) {
+    counter++;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  } else if (counter === bannerItem.length - 1) {
+    counter = 0;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter > 0) {
+    counter--;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  } else if (counter === 0) {
+    counter = bannerItem.length - 1;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  }
+});
+/*
+ setInterval
+ :반복 
+ */
+let banner = setInterval(() => {
+  if (counter < bannerItem.length - 1) {
+    counter++;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  } else if (counter === bannerItem.length - 1) {
+    counter = 0;
+    bannerBox.style.transform = "translateX(" + -size * counter + "px)";
+  }
+}, 5000);
+//1000ms = 1초
